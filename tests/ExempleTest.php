6fbf1a0d727d3a09373425e53d18556833e2b108
Administrateur <admin@mail.com>
2021-09-07 11:05:59 +0200

    Ajout d'un test d'exemple + fixtures

--- tests/ExempleTest.php
+++ tests/ExempleTest.php
@@ -6,8 +6,64 @@ use PHPUnit\Framework\TestCase;
 
 class ExempleTest extends TestCase
 {
+    /**
+     * {@inheritDoc}
+     * @see \PHPUnit\Framework\TestCase::setUp()
+     */
+    protected function setUp(): void
+    {
+        echo "\nAvant chaque test ...";
+        
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see \PHPUnit\Framework\TestCase::setUpBeforeClass()
+     */
+    public static function setUpBeforeClass(): void
+    {
+        echo "\nAvant tous les tests ...";
+        
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see \PHPUnit\Framework\TestCase::tearDown()
+     */
+    protected function tearDown(): void
+    {
+        echo "\nAprès chaque test ...";
+        
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see \PHPUnit\Framework\TestCase::tearDownAfterClass()
+     */
+    public static function tearDownAfterClass(): void
+    {
+        echo "\nAprès tous les tests ...";
+        
+    }
+    
+    public function test1(): void
+    {
+        echo "\nTest1 ...";
+        $this->assertTrue(true);
+    }
+    
+    public function test2(): void
+    {
+        echo "\nTest2 ...";
+        $this->assertTrue(true);
+        
+    }
+    
+
     public function testSomething(): void
     {
         $this->assertTrue(true);
     }
+    
+    
 }
